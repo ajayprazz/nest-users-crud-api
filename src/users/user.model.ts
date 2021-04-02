9c3a441d2b1749fdfd4675983c789a26060651d4
Ajay Prajapati <ajayprazz@gmail.com>
2021-04-02 05:46:21 +0545

    add users module and user model

---
+++ src/users/user.model.ts
@@ -0,0 +1,14 @@
+export class User {
+  constructor(
+    public id: string,
+    public name: string,
+    public gender: string,
+    public phone: string,
+    public email: string,
+    public address: string,
+    public nationality: string,
+    public dob: string,
+    public educationBackground: string,
+    public modeOfContact: string,
+  ) {}
+}
